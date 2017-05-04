b7d0c2240fd23baded724c7260a2907aac3ef5e9
Dave Rolsky <autarch@urth.org>
2017-05-04 14:53:41 -0500

    Use https for all resources

--- footer.php
+++ footer.php
@@ -11,15 +11,15 @@
 					<?php tha_footer_top(); ?>
 					<div id="page-footer" class="well clearfix">
 						<div id="site-generator"<?php echo has_nav_menu('footer-menu') ? ' class="footer-nav-menu"' : ''; ?>>
-							<a	href="<?php echo esc_url( __( 'http://wordpress.org/', 'the-bootstrap' ) ); ?>"
+							<a	href="<?php echo esc_url( __( 'https://wordpress.org/', 'the-bootstrap' ) ); ?>"
 								title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'the-bootstrap' ); ?>"
 								target="_blank"
 								rel="generator"><?php printf( _x( 'Somewhat shamefully powered by %s', 'WordPress', 'the-bootstrap' ), 'WordPress' ); ?></a>
 						</div>
-                        <a href="http://creativecommons.org/licenses/by-sa/4.0/"><img src="http://blog.urth.org/wp-content/plugins/creative-commons-configurator-1/media/cc/by-sa/4.0/80x15.png"></a>
+                        <a href="https://creativecommons.org/licenses/by-sa/4.0/"><img src="https://blog.urth.org/wp-content/plugins/creative-commons-configurator-1/media/cc/by-sa/4.0/80x15.png"></a>
                         Copyright &copy; <?php echo date('Y') ?> David Rolsky. Some rights reserved.
                         <br>
-                        All content on this site is licensed under the<br><a href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a> unless otherwise noted.
+                        All content on this site is licensed under the<br><a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a> unless otherwise noted.
 					</div><!-- #page-footer .well .clearfix -->
 					<?php tha_footer_bottom(); ?>
 				</footer><!-- #colophon -->
